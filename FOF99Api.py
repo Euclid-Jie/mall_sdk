61be30a72afd31d5045dcecdd942912daf7647ee
WeiJie Ou <ouweijie123@outlook.com>
2025-12-03 18:48:01 +0800

    完善FOF99Api类，添加净值数据上传和生成数据列表功能

--- FOF99Api.py
+++ FOF99Api.py
@@ -1,5 +1,7 @@
+import time
 import requests
-from fof99 import (
+from hashlib import md5
+from .fof99 import (
     PersonalFundPrice,
     FundInfo,
     FundPrice,
@@ -8,7 +10,6 @@ from fof99 import (
     FundMultiCompanyPrice,
     FundMultiPrice,
 )
-import numpy as np
 import pandas as pd
 
 
@@ -19,6 +20,71 @@ class FOF99Api:
 
     def __init__(self):
         pass
+
+    def update_nav_data_list_to_FOF99(self, update_nav_data_list: list[dict]):
+        # 上传净值数据到火富牛FOF99
+        sign = md5(
+            f"app_id={self.appid}&tm={int(time.time())}{self.appkey}".encode()
+        ).hexdigest()
+        url = f"https://mallapi.huofuniu.com/company_price/batch/add?app_id={self.appid}&sign={sign}&tm={int(time.time())}"
+        res = requests.post(
+            url,
+            json={
+                "price_data": update_nav_data_list,
+            },
+        )
+        print(res.content.decode("utf-8"))
+
+    @staticmethod
+    def generate_nav_data_list(sub_nav_data_df: pd.DataFrame) -> list[dict]:
+        # 挨个产品添加
+        product_code_list = sub_nav_data_df["产品代码"].unique()
+        update_nav_data_list = []
+        for code in product_code_list:
+            single_nav_data = sub_nav_data_df[sub_nav_data_df["产品代码"] == code]
+            result = [
+                {
+                    "date": row["日期"],
+                    "price": str(row["单位净值"]),
+                    "cumulative_nav_withdrawal": str(row["累计净值"]),
+                }
+                for _, row in single_nav_data.iterrows()
+            ]
+            update_nav_data_list.append(
+                {
+                    "code": code,
+                    "nets": result,
+                }
+            )
+        return update_nav_data_list
+
+    def update_nav_to_FOF99(self, nav_data_for_update: pd.DataFrame) -> list[dict]:
+        """
+        转换数据格式并上传到FOF99
+        Parameters
+        """
+        # 防止修改原数据
+        nav_data_for_update = nav_data_for_update.copy()
+        assert nav_data_for_update.shape[0] > 0, "没有需要更新的数据！"
+        # 确认字段
+        expected_columns = ["产品代码", "日期", "单位净值", "累计净值"]
+        assert set(nav_data_for_update.columns) > set(
+            expected_columns
+        ), "字段不匹配，请检查！期望字段: {}，实际字段: {}".format(
+            expected_columns, nav_data_for_update.columns.tolist()
+        )
+        # 日期格式转换
+        assert (
+            nav_data_for_update["日期"].dtype == "datetime64[ns]"
+        ), "日期格式错误，请检查！"
+        nav_data_for_update["日期"] = nav_data_for_update["日期"].dt.strftime(
+            "%Y-%m-%d"
+        )
+        # 分多个chuck上传
+        for i in range(0, nav_data_for_update.shape[0], 1000):
+            sub_nav_data_df = nav_data_for_update.iloc[i : i + 1000, :]
+            update_nav_data_list = self.generate_nav_data_list(sub_nav_data_df)
+            self.update_nav_data_list_to_FOF99(update_nav_data_list)
 
     def get_fund_info(self, reg_code: str = "SVZ009"):
         req = FundInfo(self.appid, self.appkey)  # 请求对
